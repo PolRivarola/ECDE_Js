7e0fae25e34c1c4db7ebc03bb8f254c5c14a1d52
PolRivarola <82118200+PolRivarola@users.noreply.github.com>
2021-10-29 12:12:20 -0300

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,151 @@
+// Objeto Atril
+class Atril {
+  constructor(mail, color, vinilo, envio, precio) {
+    this.mail = mail;
+    this.color = color;
+    this.vinilo = vinilo;
+    this.envio = envio;
+    this.precio = precio;
+  }
+}
+
+// Declaramos variables y link
+let color;
+let vinilo;
+URLGET = "../data/compras.json";
+let precio = 0;
+let total = 0;
+let arrayPedidos = [];
+
+// Funcion GET de productos
+$(".colorAt").mouseover(function () {
+  let datoDe = $(this).next().prop("innerHTML");
+  $.get(URLGET, function (respuesta, estado) {
+    if (estado === "success") {
+      let misDatos = respuesta;
+      for (const dato of misDatos) {
+        if (dato.title == datoDe) {
+          $("#dato").append(`
+                <div class="datosDisplay" styile="flex-flow","column">
+                <h3 >${dato.title}</h3>
+                <p  >${dato.body}</p>
+                </div>
+                `);
+        }
+      }
+    }
+    $("#dato").fadeIn("slow");
+  });
+});
+
+
+// Animaciones y pasos de catalogo
+$(".colorAt").mouseout(function () {
+  $("#dato").html("");
+  $("#dato").css("display", "none");
+});
+
+$(".colorAt").click(function () {
+  color = $(this).next().prop("innerHTML");
+  $(".primerPaso").fadeOut("fast");
+  $(".segundoPaso").fadeIn("slow");
+});
+
+$(".viniloAt").click(function () {
+  vinilo = $(this).next().prop("innerHTML");
+  $(".segundoPaso").fadeOut("fast");
+  $(".tercerPaso").fadeIn("slow");
+});
+
+$("#btnPedir").click(function () {
+  let mail = $("#mail")[0].value;
+  if (mail == "") {
+    alert("Porfavor ingrese un mail!");
+  } else {
+    guardar();
+  }
+});
+
+$("#btnSi").click(function () {
+  $(".cuartoPaso").fadeOut("fast");
+  $(".primerPaso").fadeIn("slow");
+});
+
+$("#btnNo").click(function () {
+  $(".cuartoPaso").fadeOut("fast");
+  $(".quintoPaso").fadeIn("slow");
+  mostrarCompras();
+});
+
+// Muestra compras del usuario
+function mostrarCompras() {
+  $("#mostarPed").prepend(
+    `<div class="mensaje">
+    <p class="pedCon"> Estos son tus pedidos...cierto?</p>
+    </div>`
+  );
+  for (let i in arrayPedidos) {
+    if (i == 0) {
+      $(".carouselcompras").append(
+        `<div class="carousel-item active">
+        <div class="displayMad pedFlex">
+        <p> Color: ${arrayPedidos[i].color}</p>
+        <p> Vinilo: ${arrayPedidos[i].vinilo}</p>
+        <p> Anillo?: ${arrayPedidos[i].envio}</p>
+        <p> Precio: ${arrayPedidos[i].precio}</p>
+          </div>
+        </div>`
+      );
+    } else {
+      $(".carouselcompras").append(
+        `<div class="carousel-item ">
+        <div class="displayMad pedFlex" >
+        <p> Color: ${arrayPedidos[i].color}</p>
+        <p> Vinilo: ${arrayPedidos[i].vinilo}</p>
+        <p> Anillo?: ${arrayPedidos[i].envio}</p>
+        <p> Precio: ${arrayPedidos[i].precio}</p>
+          </div>
+        </div>`
+      );
+    }
+  }
+}
+
+
+// Funcion para guardar la compra y actualizar el localStorage
+function guardar() {
+  let anillo = $("#anillo").val();
+  let mail = $("#mail").val();
+
+  if (anillo == "Si") {
+    precio = 2800;
+  } else {
+    precio = 2500;
+  }
+
+  if (anillo == "Si") {
+    total += 2800;
+  } else {
+    total += 2500;
+  }
+
+  let pedido = new Atril(mail, color, vinilo, anillo, precio);
+  let numPed = localStorage.length + 1;
+  let Numpedido = "pedido" + numPed.toString();
+  arrayPedidos.push(pedido);
+  localStorage.setItem(Numpedido, JSON.stringify(pedido));
+
+  $("#ready").html("");
+  $("#ready").append(`
+    <div class="mensaje">
+    <p class="pedCon"> Pedido confirmado!</p>
+    <p> Color: ${pedido.color}</p>
+    <p> Vinilo: ${pedido.vinilo}</p>
+    <p> Anillo?: ${pedido.envio}</p>
+    <p> Precio: ${pedido.precio}</p>    
+    <p> Total: ${total}</p>
+    </div>`);
+
+  $(".tercerPaso").fadeOut("fast");
+  $(".cuartoPaso").fadeIn("slow");
+}
